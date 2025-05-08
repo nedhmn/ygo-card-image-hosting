65edf725c688e20931c8752ab9719ae818a9a409
nedhmn <hermannned@gmail.com>
2025-05-08 03:36:16 -0400

    chore: setup

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type     = string
+  nullable = false
+}
+
+variable "aws_access_key_id" {
+  type     = string
+  nullable = false
+}
+
+variable "aws_secret_access_key" {
+  type     = string
+  nullable = false
+}
