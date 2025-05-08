5a42bbd24096bb847f666714688ffd8815528010
nedhmn <hermannned@gmail.com>
2025-05-08 09:10:06 -0400

    chore: changd terraform output for cloudfront url

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,7 @@ module "card_cdn" {
   bucket_name = var.bucket_name
 }
 
-output "cloudfront_domain" {
-  description = "CloudFront distribution domain name"
-  value       = module.card_cdn.cloudfront_domain_name
-}
-
-output "s3_bucket_arn" {
-  description = "ARN of the S3 bucket"
-  value       = module.card_cdn.s3_bucket_arn
+output "cloudfront_url" { 
+  description = "CloudFront distribution url" 
+  value       = module.card_cdn.cloudfront_url
 }
